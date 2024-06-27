2cfe30b0674faf9d172f3730746e039749c65326
zhuddan <yobaci@163.com>
2024-06-27 14:28:42 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-import plugin from '@tailwindcss/line-clamp'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -58,6 +57,6 @@ export default {
     },
   },
   plugins: [
-    plugin,
+    // plugin,
   ],
 }
