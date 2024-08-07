7fed30fb9c7fb3dc0a6f4a7d97ff8ab9e7c4f6fd
zhuddan <yobaci@163.com>
2024-08-07 11:02:48 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import React from '@vitejs/plugin-react'
 import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    React(),
-    Icons({
-      compiler: 'jsx',
-      jsx: 'react',
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, './')
+  return {
+    plugins: [
+      React(),
+      Icons({
+        compiler: 'jsx',
+        jsx: 'react',
+      }),
+    ],
+    server: {
+      port: Number(env.VITE_APP_PORT),
+      host: true,
+    },
+  }
 })
